4db3180729f128943b3bc4e6ed2e3a4405a90588
rkrmr33 <rkrmr33@users.noreply.github.com>
2024-11-17 08:06:37 +0000

    chore: bump chart version to '0.1.57'

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "chart_version" {
   type        = string
   description = "Specifies the version of the Helm chart to be installed"
-  default     = "0.1.56"
+  default     = "0.1.57"
   nullable    = false
 }
 
